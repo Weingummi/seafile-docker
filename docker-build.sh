a95d5bebc650e28f736dec949171aa729c424414
Weingummi <weingummi@protonmail.com>
2018-01-13 00:36:54 +0100

    Added version forwarding for docker-build.sh

--- docker-build.sh
+++ docker-build.sh
@@ -3,17 +3,19 @@
 cd `dirname $0`
 
 ARCH=`uname -m`
-IMAGE="sunx/seafile"
+IMAGE="seafile"
+VERSION="6.2.4"
 
 if `echo $ARCH | grep -q arm`; then
 	#Use armhf/alpine as base image instead of alpine
-	sed -r 's,(FROM)\s+(alpine),\1 armhf/\2,' < ./docker/Dockerfile > ./docker/Dockerfile.arm
+	sed -r 's,(FROM)\s+(alpine),\1 armhf/\2,;s,(SEAFILE_VERSION=")[0-9.]*("),\1'$VERSION'\2,' < ./docker/Dockerfile > ./docker/Dockerfile.arm
 
-	docker build -t $IMAGE -f ./docker/Dockerfile.arm ./docker/
+	docker build -t ${IMAGE}:${VERSION} -f ./docker/Dockerfile.arm ./docker/
 	#&& rm -rf ./build
 else
 	if `echo $ARCH | grep -q x86_64`; then
-		docker build -t $IMAGE ./docker/
+		sed -r -i 's,(SEAFILE_VERSION=")[0-9.]*("),\1'$VERSION'\2,' ./docker/Dockerfile
+		docker build -t ${IMAGE}:${VERSION} ./docker/
 	else 
 		echo "Error: Architecture $ARCH isn't supported"
 		exit 1
